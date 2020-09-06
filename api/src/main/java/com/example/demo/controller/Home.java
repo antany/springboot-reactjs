c6eb371dd698d75abfc83ca47f62c7494851e27f
antany <antany84@gmail.com>
2020-09-06 00:34:09 -0400

    Home Rest Controller Added

---
+++ api/src/main/java/com/example/demo/controller/Home.java
@@ -0,0 +1,13 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Home {
+
+	@RequestMapping("/home")
+	public String home() {
+		return "Home Controller - Home Method";
+	}
+}
